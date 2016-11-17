3f37bc255edc8fe5dda5224c24a33478df526e45
Usuario <Usuario@PC-10>
2016-11-17 18:53:09 -0300

    ok

--- PruebaEF_Consola/Data/TestEFContext.cs
+++ PruebaEF_Consola/Data/TestEFContext.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Data.Entity;
+using Entidades;
 namespace Data
 {
-    class TestEFContext 
+    public class TestEFContext : DbContext
     {
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Perfil> Perfiles { get; set; }
     }
 }
--- PruebaEF_Consola/PruebaEF_Consola/Program.cs
+++ PruebaEF_Consola/PruebaEF_Consola/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data;
+using Entidades;
 
 namespace PruebaEF_Consola
 {
@@ -10,7 +12,8 @@ namespace PruebaEF_Consola
     {
         static void Main(string[] args)
         {
-      
+            TestEFContext ctx = new TestEFContext();
+            ctx.Usuarios.SqlQuery; 
         }
     }
 }
